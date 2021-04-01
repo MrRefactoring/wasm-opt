0a1de72e887f33c53430e5bc97b1442ebf6d6bb8
Vladislav Tupikin <mrrefactoring@yandex.ru>
2021-04-01 16:15:52 +0300

    fixes

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+### 1.1.0
+- Added linux and macOs support
+
+### 1.0.0
+- Release
--- bin/wasm-opt.js
+++ bin/wasm-opt.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
 const path = require('path');
 const cp = require('child_process');
 
+const rootDir = path.resolve('../wasm-opt/bin/wasm-opt' + process.platform === 'win32' ? '.exe' : '');
+
 const spawned = cp.spawn(
-  path.join(__dirname, 'wasm-opt'),
+  fs.existsSync(rootDir) ? rootDir : path.join(__dirname, 'wasm-opt' + process.platform === 'win32' ? '.exe' : ''),
   process.argv.slice(2),
   { cwd: './', shell: process.platform === 'win32' }
 );
